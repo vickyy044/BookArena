2277f6f641954c00e427acfd78705e6f353194de
vickyy044 <43821289+vickyy044@users.noreply.github.com>
2020-07-02 10:18:59 +0530

    - Hamburger Icon added  - Navigation Drawer completed

--- app/src/main/java/com/example/bookarena/MainActivity.kt
+++ app/src/main/java/com/example/bookarena/MainActivity.kt
@@ -2,11 +2,52 @@ package com.example.bookarena
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.MenuItem
+import android.widget.FrameLayout
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.circularreveal.coordinatorlayout.CircularRevealCoordinatorLayout
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var drawerLayout : DrawerLayout
+    lateinit var coordinatorLayout: CoordinatorLayout
+    lateinit var toolbar : Toolbar
+    lateinit var navigationView : NavigationView
+    lateinit var frameLayout : FrameLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        drawerLayout = findViewById(R.id.drawer_layout)
+        coordinatorLayout = findViewById(R.id.coordinator_layout)
+        toolbar = findViewById(R.id.toolbar)
+        navigationView = findViewById(R.id.navigation_view)
+        frameLayout = findViewById(R.id.frame_layout)
+        setUpToolbar()
+
+        val actionBarDrawerToggle = ActionBarDrawerToggle(this@MainActivity,drawerLayout,R.string.open_drawer,R.string.close_drawer)
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+    }
+
+    fun setUpToolbar(){
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = "Book Arena"
+        supportActionBar?.setHomeButtonEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+         if(id== android.R.id.home)
+             drawerLayout.openDrawer(GravityCompat.START)
+        return super.onOptionsItemSelected(item)
     }
 }
